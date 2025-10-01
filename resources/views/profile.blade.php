f8e01e63e975db000fdede35a84abafc307b0890
Dzakwan435 <dzakwanabbas018@gmail.com>
2025-10-01 18:32:29 +0700

    pengenalan view laravel

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>profile</title>
+</head>
+<body>
+  h1>halaman profile</h1>
+  <p>nama saya adalah {{ $nama }} </p><br>
+  <p>umur saya {{ $umur }} </p><br>
+  <p>alamat saya di {{ $alamat }} </p><br>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,54 +3,16 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    // return view('welcome')
-    return "
-    <h1> halaman home </h1>
-    <a href='/about'>About</a><br>
-    <a href='/profile'>profile</a><br>
-    <a href='/materi-laravel-10-part-2'>materi-laravel-10-part-2</a>
-       
-    ";
+    return view('home');
 });
 
 Route::get('/about', function () {
-    // return view('welcome')
-    return "
-    <h1> halaman about </h1>
-    <a href='/'>home</a><br>
-    <a href='/profile'>profile</a><br>
-    <a href='/materi-laravel-10-part-2'>materi-laravel-10-part-2</a>
-       
-    ";
+    return view('about');
 });
 
 Route::get('/profile', function () {
-    // return view('welcome')
-    return "
-    <h1> halaman profile </h1>
-    <a href='/'>home</a><br>
-    <a href='/about'>About</a><br>
-    <a href='/materi-laravel-10-part-2'>materi-laravel-10-part-2</a>
-       
-    ";
-});
-
-Route::get('/materi-laravel-10-part-2', function () {
-    // return view('welcome')
-    $nama = "dzakwan Abbas";
-    $alamat = "marendal JL sumber Amal";
+    $nama = "Dzakwan abbas";
     $umur = 20;
-    $jurusan = "sistem informasi";
-    $fakultas = "sains & teknologi";
-    return " 
-    <h1> halo nama saya $nama</h1>
-    <h2> alamat saya $alamat</h2>
-    <h3> umur saya $umur</h3>
-    <h3> saya kuliah di jurusan $jurusan</h3>
-    <h3> fakultas saya $fakultas</h3>
-    <a href='/'>home</a><br>
-    <a href='/about'>About</a><br>
-    <a href='profile'>profile</a><br>
-       
-    ";
+    $alamat = "medan";
+    return view('profile', compact('nama', 'umur', 'alamat',));
 });
