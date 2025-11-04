080ac0f5bfd5514a2e56fd4a39c37355a9920e22
Dzakwan435 <dzakwanabbas018@gmail.com>
2025-11-04 15:46:11 +0700

    uts 3 tambahan yang kelewat

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,18 @@ Route::get('/mahasewa', function () {
 Route::get('/uts', function () {
     return view('mahasiswa.uts');
 });
+
+Route::get('/produk', function () {
+    $products = [
+        ['kode' => 'BRG001', 'nama' => 'PILPEN', 'jenis' => 'ALAT TULIS', 'harga' => 20000],
+        ['kode' => 'BRD002', 'nama' => 'BUKU', 'jenis' => 'ALAT TULIS', 'harga' => 15000],
+    ];
+    
+    return view('mahasiswa.produk', ['products' => $products]);
+});
+
+Route::get('/tambah', function () {
+    return view('mahasiswa.tambah');
+});
+
+    
