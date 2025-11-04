cee8ab6b2e12d47e2316bd4961f17eb247bb4918
Dzakwan435 <dzakwanabbas018@gmail.com>
2025-11-04 14:54:52 +0700

    uts no 2

--- resources/views/mahasiswa/uts.blade.php
+++ resources/views/mahasiswa/uts.blade.php
@@ -31,6 +31,7 @@
         <form class="d-flex mt-3" role="search">
           <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
           <button class="btn btn-outline-success" type="submit">Search</button>
+          <p>soal no 2 uts tadi ke commit bareng no 1</p>
         </form>
       </div>
     </div>
